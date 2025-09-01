c449d7ed94c84e70ed88048a932b2d0fd99e27f2
TE23D-Eriksson-David <david.eriksson2@elev.ga.ntig.se>
2025-09-01 11:30:15 +0200

    start

---
+++ src/App.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int[] bussPlatser = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+        int[] pernsonumerPlatser = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}; 
+        String[] NamnPlatser = {null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null,null};
+        boolean Klar = false;
+        
+
+        System.out.println("1. Boka en plats.\n2. Se obokade platser.\n3. Se Bokning\n 4. Ta bort bokning\n5. Se vinsten av bokade platser.");
+
+    }
+}
